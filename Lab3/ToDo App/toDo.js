8aea51b40aafb3b18225e8851af1a61e7c969c15
Danila Khlebokazov <danilik_2003@mail.ru>
2023-02-12 18:32:19 +0600

    toDo list 60%
    main js code done

---
+++ Lab3/ToDo App/toDo.js
@@ -0,0 +1,20 @@
+document.querySelector('#add-button').onclick = function(){
+    if(document.querySelector('#main-input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+    else{
+        document.querySelector('#toDo-list-block').innerHTML += `
+            <div class="toDo-task"> 
+                <input type="checkbox" name="" id=""> 
+                ${document.querySelector('#main-input').value}
+                <button class="delete-button">Delete</button>
+            </div>
+        `;
+        var current_tasks = document.querySelectorAll(".delete-button");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+    }
+}
