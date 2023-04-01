d94d3a253fe8955911c30388e13ef124f6859ec1
Danila Khlebokazov <danilik_2003@mail.ru>
2023-04-01 18:56:06 +0600

    Lab8 done

--- Lab8/shop_back/api/urls.py
+++ Lab8/shop_back/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
 from api import views
 urlpatterns = [
-    # path('products/', views.product_list),
-    # path('categories/', views.categories_list),
-    # re_path(r'^categories/([0-5])/$',views.categories_by_id),
-    # re_path(r'^products/(\d{1,2})/$',views.product_by_id)
+    path('products/', views.product_list),
+    path('categories/', views.categories_list),
+    re_path(r'^categories/([0-5])/$',views.categories_by_id),
+    re_path(r'^products/(\d{1,2})/$',views.product_by_id),
+    re_path(r'^categories/(\d{1,2})/products/$', views.products_by_category)
 ]
