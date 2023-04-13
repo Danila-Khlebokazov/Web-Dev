a3a2738ba67f8dcf2ca61cc3dde7c53c0c817a1c
Danila Khlebokazov <danilik_2003@mail.ru>
2023-04-13 16:08:44 +0600

    Lab10 70%

---
+++ Lab10/hhBack/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, re_path
+from api import views
+urlpatterns = [
+    path('companies/', views.companies_list),
+    path('vacancies/', views.vacancies_list),
+    re_path(r'^companies/([0-5])/$',views.company_by_id),
+    re_path(r'^vacancies/(\d{1,2})/$',views.vacancy_by_id),
+    re_path(r'^companies/(\d{1,2})/vacancies/$', views.vac_by_company),
+    re_path(r'vacancies/top_ten/$', views.vac_top_ten)
+]
